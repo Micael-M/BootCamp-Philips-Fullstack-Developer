97abf76ff9bdf46c045a2047fa411abcabacce0f
Micael Santos <micaelmaicon@hotmail.com>
2022-04-03 19:43:46 -0300

    I finish creating the cactus

--- recreating-google-dino-game/scripts/script.js
+++ recreating-google-dino-game/scripts/script.js
@@ -1,4 +1,5 @@
 const DINO_PLAYER = document.querySelector('.dino');
+const DESERT = document.querySelector('.background');
 let isJumping = false;
 
 // Site used to find the kaycode of each key:
@@ -33,7 +34,33 @@ function jump() {
     // going up
       position += 20;
       DINO_PLAYER.style.bottom = `${position}px`;
-  },20);
+  }, 20);
 }
+
+// Create cactus elements
+function createCactus() {
+  const CACTUS = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTimer = Math.random() * 4500  
+
+  CACTUS.classList.add('cactus');
+  CACTUS.style.left = `${cactusPosition}px`
+  DESERT.appendChild(CACTUS);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      DESERT.removeChild(CACTUS);
+      return;
+    }
+    cactusPosition -= 12;
+    CACTUS.style.left = `${cactusPosition}px`;
+  }, 20);
+
+// 
+  setTimeout(createCactus, randomTimer);
+}
+
+createCactus();
 // take the jump key
 document.addEventListener('keydown', handleKeyDown);
